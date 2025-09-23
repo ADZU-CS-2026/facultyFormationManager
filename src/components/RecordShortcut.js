83b283195e0b3d93a9f7b8c395e8f8580bb5d43a
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-09-24 02:51:39 +0800

    lazy commit

--- src/components/RecordShortcut.js
+++ src/components/RecordShortcut.js
@@ -11,7 +11,7 @@ export default function RecordShortcut() {
       <div className="text-gray1 border-bottom p-2 px-3 fs-6 d-flex justify-content-between align-items-center">
         <div>
           <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" className="fs-6 me-1" />
-          Records Shortcut
+          Record Shortcuts
         </div>
         <span
           className="cursor-pointer"
@@ -33,16 +33,16 @@ export default function RecordShortcut() {
         style={{ overflow: "hidden" }}
       >
         <div className="p-2 fs-5 d-flex flex-column gap-2">
-          <Link href="/records/all" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-table-list" className="fs-6 me-1" />All Records</Link>
-          <Link href="/records/admin" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-user-tie" className="fs-6 me-1" />Administrators</Link>
-          <Link href="/records/ffp" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-person" className="fs-6 me-1"/>Freshmen Formation Program</Link>
-          <Link href="/records/conn" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-user-nurse" className="fs-6 me-1"/>College of Nursing</Link>
-          <Link href="/records/csite" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-microchip" className="fs-6 me-1"/>College of Science and Information Technology and Engineering</Link>
-          <Link href="/records/sed" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-school" className="fs-6 me-1"/>School of Education</Link>
-          <Link href="/records/sla" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-palette" className="fs-6 me-1"/>School of Liberal Arts Retreat</Link>
-          <Link href="/records/sma" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-receipt" className="fs-6 me-1"/>School of Management and Accountancy</Link>
-          <Link href="/records/cs" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-handshake-angle" className="fs-6 me-1"/>Central Services</Link>
-          <Link href="/records/ppo" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start"><FontAwesomeIcon icon="fa-solid fa-person-military-pointing" className="fs-6 me-1"/>Physical Plant Personnel Profile</Link>
+          <Link href="/records/all" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-table-list" className="fs-6 me-1" />All Records</Link>
+          <Link href="/records/admin" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-user-tie" className="fs-6 me-1" />Administrators</Link>
+          <Link href="/records/ffp" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-person" className="fs-6 me-1"/>Freshmen Formation Program</Link>
+          <Link href="/records/conn" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-user-nurse" className="fs-6 me-1"/>College of Nursing</Link>
+          <Link href="/records/csite" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-microchip" className="fs-6 me-1"/>College of Science and Information Technology and Engineering</Link>
+          <Link href="/records/sed" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-user-graduate" className="fs-6 me-1"/>School of Education</Link>
+          <Link href="/records/sla" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-palette" className="fs-6 me-1"/>School of Liberal Arts Retreat</Link>
+          <Link href="/records/sma" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-receipt" className="fs-6 me-1"/>School of Management and Accountancy</Link>
+          <Link href="/records/cs" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-handshake-angle" className="fs-6 me-1"/>Central Services</Link>
+          <Link href="/records/ppo" className="btn btn-white1 border w-100 text-lightblue fw-bold fs-6 text-start shadow-sm"><FontAwesomeIcon icon="fa-solid fa-person-military-pointing" className="fs-6 me-1"/>Physical Plant Personnel Profile</Link>
         </div>
       </motion.div>
     </div>
