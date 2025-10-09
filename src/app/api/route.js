0ab4e560c16b7a8bd6bfe92445745f8b9193c1fb
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-10-10 03:08:02 +0800

    lazy commit

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -13,3 +13,19 @@ export async function GET(){
         return NextResponse.json({message: `Server Error: ${err}`}, {status: 500});
     }
 }
+
+export async function PATCH(req){
+    try{
+        const {id, newPass} = await req.json();
+        const parsedID = parseInt(id);
+
+        const [result] = await pool.execute("UPDATE adminaccount SET password = ? WHERE id = ?", [newPass, parsedID]);
+        if(result.affectedRows > 0){
+            return NextResponse.json({message: "Updated password!"}, {status: 200});
+        }
+        return NextResponse.json({message: "Password not updated!"}, {status: 404});
+    }
+    catch(err){
+        return NextResponse.json({message: `Server Error: ${err}`}, {status: 500});
+    }
+}
