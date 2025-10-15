a2ccb81ba63087e56debb51e478d7bcf57d1c9c8
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-10-15 22:59:56 +0800

    lazy commit

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -226,7 +226,7 @@ export default function Home() {
                 Forgot your Password <FontAwesomeIcon icon={faCircleQuestion} />
               </div>
               <Turnstile
-                sitekey="1x00000000000000000000AA" //"1x00000000000000000000AA" for production
+                sitekey="1x00000000000000000000AA" // production.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY for production
                 onVerify={(token) => setToken(token)}
               />
             </div>
