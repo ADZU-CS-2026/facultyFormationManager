e84bb2af8f8e80dcadca4c1a50de458d6b165bda
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-10-27 00:25:33 +0800

    lazy commit

--- src/app/api/auth/cloudflare-turnstile/route.js
+++ src/app/api/auth/cloudflare-turnstile/route.js
@@ -4,20 +4,16 @@ export async function POST(req){
 
     try{
         
-        // const { token } = await req.json();
-        // const secret = process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
-        // const res = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/x-www-form-urlencoded"
-        //     },
-        //     body: `secret=${secret}&response=${token}`
-        // });
-        // const data = await res.json();
-
-        // if(process.env.NODE_ENV !== "production"){
-        //     return NextResponse.json({success: true}, {status: 200});
-        // }
+        const { token } = await req.json();
+        const secret = process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
+        const res = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: `secret=${secret}&response=${token}`
+        });
+        const data = await res.json();
         return NextResponse.json({success: true}, {status: 200});
         
     }   
