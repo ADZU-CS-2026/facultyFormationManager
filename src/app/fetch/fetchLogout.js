e90bad09ec7392eea9508bf6a5d957fead04dfa0
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-11-23 19:56:15 +0800

    lazy commit

--- src/app/fetch/fetchLogout.js
+++ src/app/fetch/fetchLogout.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function fetchLogout() {
   try {
-    const res = await axios.post("/api/auth/logout");
+    const res = await axios.post("/api/auth/logout", { withCredentials: true });
     return res;
   } catch (err) {
     throw new Error(err.message);
