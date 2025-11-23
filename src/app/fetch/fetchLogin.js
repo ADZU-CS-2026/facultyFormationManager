a40d006c95805f7560fb3ba10f08356b18eb40d1
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-11-23 19:36:34 +0800

    lazy commit

--- src/app/fetch/fetchLogin.js
+++ src/app/fetch/fetchLogin.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 export default async function fetchLogin(id, password) {
   try {
-    const res = await axios.post(
-      "/api/auth/login",
-      { id, password },
-      { validateStatus: () => true }
-    );
+    const res = await axios.post("/api/auth/login", { id, password });
     return res;
   } catch (err) {
     throw new Error(err.message);
