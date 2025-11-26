4beef455dd5a665395603f8064acb64676273086
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-11-26 21:51:10 +0800

    lazy commit

--- src/app/api/auth/cloudflare-turnstile/route.js
+++ src/app/api/auth/cloudflare-turnstile/route.js
@@ -2,19 +2,23 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    // const { token } = await req.json();
-    // const secret = process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
-    // const res = await fetch(
-    //   "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/x-www-form-urlencoded",
-    //     },
-    //     body: `secret=${secret}&response=${token}`,
-    //   }
-    // );
-    // const data = await res.json();
+    if (process.env.NODE_ENV === "production") {
+      const { token } = await req.json();
+      const secret = process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
+      const res = await fetch(
+        "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: `secret=${secret}&response=${token}`,
+        }
+      );
+      const data = await res.json();
+      return NextResponse.json(data, { status: 200 });
+    }
+
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (err) {
     return NextResponse.json(
