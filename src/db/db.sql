3d86bf212218c36562d16e9b4083dc867b6e779a
Qhaleed <reslykadiri@gmail.com>
2025-11-28 13:43:25 +0800

    feat: new query for non uniqe role and password for adminaccount table;

--- src/db/db.sql
+++ src/db/db.sql
@@ -55,8 +55,8 @@ CREATE TABLE certificates (
 -- Admin Account Table (for system login)
 CREATE TABLE adminaccount(
     id INT NOT NULL PRIMARY KEY,
-    role VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL UNIQUE
+    role VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL
     
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
